c848bc01c93cb9a4fcbffada6f29e68c51937766
Ryan Shaver <ryan.shaver@live.com>
2020-11-24 14:52:37 -0600

    Beginning the For loop and If/Else logic

--- guess-the-number.py
+++ guess-the-number.py
@@ -13,3 +13,9 @@ print("Here's how to play:")
 print("I'm thinking of a number between 1 and 10")
 print("You have 4 guesses to get it right")
 guess = input("What is your first guess? ")
+
+number = int("")
+
+for i in range(1,11):
+    if guess == number:
+        print("Wow! You guessed it right!")
