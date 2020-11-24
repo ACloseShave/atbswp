b24c2aa24ded546fe935de642092cdd4d09471ee
Ryan Shaver <32579646+ACloseShave@users.noreply.github.com>
2020-11-24 17:54:35 -0600

    Code Cleanup
    Code cleanup, fixed For loop, cut extra content

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,36 +1,31 @@
 #!/bin/env python3
 
-# Number guessing game
 from random import randint as random_num
+secret_number = random_num(1, 20)
 
 
 print("Hello! This is the random number game. First, ")
 name = input("What is your name? ")
-secret_number = random_num(1, 20)
-
-
-print("Hello " + name + "!")
-print("Here's how to play:")
+print("Hello " + name + "! Here's how to play:")
 
 
 #  Begin game with rules
 print("I'm thinking of a number between 1 and 20")
 print("You have 4 guesses to get it right")
 
-guess = int(input("What is your first guess? "))
-
-def guesses_remaining():
-    print("You have " + str(guess - 1) + " guesses remaining"
-
 
 #  Begin guess/actual comparison loop
-for attempt_number in range(4, 0, -1):
+for attempt_number in range(4, 0, -1):    
+    guess = int(input("What's your first? "))
+
     if guess > secret_number:
-            print("That's too high!")
+        print("That's too high!")
     elif guess < secret_number:
-            print("That's too low!")
+        print("That's too low!")
     else:
         print("That's the number!")
         break
-    guesses_remaining()
-
+    
+    guesses_remaining = str(attempt_number - 1)
+    
+    print("You have " + guesses_remaining + " guesses remaining.")
