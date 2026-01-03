0eb56f6aea150a59240482b9f47cad6a82aef0a1
ACloseShave <sgtavers@gmail.com>
2026-01-03 15:49:36 -0600

    Practicing For vs While loops

---
+++ practice_with_loops.py
@@ -0,0 +1,12 @@
+# Practicing loops for Ch. 3 exercises
+
+
+# For loop
+for num in range(1, 11):
+    print(f"For loop: {num}")
+
+# While loop
+i = 1
+while i < 11:
+    print(f"While loop: {i}")
+    i += 1
