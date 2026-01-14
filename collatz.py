c43d0fc38c92a8e6c598e39e13445f420d1cbf62
ACloseShave <sgtavers@gmail.com>
2026-01-14 13:44:29 -0600

    Collatz sequence fun with try...except

---
+++ collatz.py
@@ -0,0 +1,21 @@
+def collatz(number):
+    if number % 2 == 0:
+        result = number // 2
+        print(result)
+        return result
+    else:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+while True:
+    print('Enter an integer:')
+    try:
+        user_input = int(input())
+        if user_input < 0:
+            raise ValueError('Positive integers only')
+        while user_input != 1:
+            user_input = collatz(user_input)
+        break
+    except ValueError:
+        print('Error: Invalid value. You must enter a positive whole number only.')
